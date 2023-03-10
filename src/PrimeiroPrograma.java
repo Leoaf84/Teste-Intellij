5e69bf6b4923c638e26410f8de7ba4e1689c8e8e
perkles <leonardo.abel.f@hotmail.com>
2023-03-10 12:09:47 -0300

    first commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+import New.Gato;
+import org.w3c.dom.ls.LSOutput;
+
+import java.awt.*;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+
+
+}
